c83ef6406fcc7656728a78f4ca4cb53979719522
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-17 13:22:47 +0900

    config: dotEnv설정

--- webpack/webpack.config.common.js
+++ webpack/webpack.config.common.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: { index: './src/index.tsx' },
@@ -34,5 +35,6 @@ module.exports = {
       template: './src/index.html',
     }),
     new ForkTsCheckerWebpackPlugin(),
+    new Dotenv(),
   ],
 };
