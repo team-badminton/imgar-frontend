4199c55e7d87f19d322bd545915efdbe76159e5f
HyunJung <HyunJungC.Dev@gmail.com>
2021-09-23 15:57:42 +0900

    fix: ImageCard 컴포넌트 마크업
    - article을 a 태그로 감싸서 해당 포스트에 대한 링크로 가도록 수정

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -4,37 +4,41 @@ import { ReactComponent as UpIconSVG } from '../../assets/Icon/UpIcon.svg';
 import { ReactComponent as CommentIconSVG } from '../../assets/Icon/commentIcon.svg';
 import { ReactComponent as ViewIconSVG } from '../../assets/Icon/viewIcon.svg';
 import { Image } from '..';
+import { StyledImageCard, StyledImageCardFooter } from './ImageCard.styled';
 
 interface ImageCardProps {
   postInfo: PostInfo;
+  ImageCardWidth: string | number;
 }
 
-export default function ImageCard({ postInfo }: ImageCardProps): ReactElement {
+export default function ImageCard({ postInfo, ImageCardWidth }: ImageCardProps): ReactElement {
   const thumbnail = postInfo.images[0];
   const { title, upCount, downCount, commentCount, views } = postInfo;
   const ALT_TEXT = '사용자 혹은 AI가 작성한 이미지에 대한 구체적인 설명';
   return (
-    <article>
-      <Image
-        alt={ALT_TEXT}
-        objectFit="contain"
-        src={`https://i.imgur.com/${thumbnail.id}_d.webp?maxwidth=${thumbnail.imageWidth}&shape=thumb&fidelity=high`}
-      />
-      <h3>{title}</h3>
-      <footer>
-        <div>
-          <UpIconSVG />
-          <span>{upCount - downCount}</span>
-        </div>
-        <div>
-          <CommentIconSVG />
-          <span>{commentCount}</span>
-        </div>
-        <div>
-          <ViewIconSVG />
-          <span>{views}</span>
-        </div>
-      </footer>
-    </article>
+    <a href="/postUrl">
+      <article>
+        <Image
+          alt={ALT_TEXT}
+          objectFit="contain"
+          src={`https://i.imgur.com/${thumbnail.id}_d.webp?maxwidth=${thumbnail.imageWidth}&shape=thumb&fidelity=high`}
+        />
+        <h3>{title}</h3>
+        <footer>
+          <div>
+            <UpIconSVG />
+            <span>{upCount - downCount}</span>
+          </div>
+          <div>
+            <CommentIconSVG />
+            <span>{commentCount}</span>
+          </div>
+          <div>
+            <ViewIconSVG />
+            <span>{views}</span>
+          </div>
+        </footer>
+      </article>
+    </a>
   );
 }
