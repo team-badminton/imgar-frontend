edf8e33f3f0e15b4979630aec76b4a592a6ab079
HyunJung <HyunJungC.Dev@gmail.com>
2021-09-23 14:13:04 +0900

    feat: ImageCard 컴포넌트 마크업
    - Image 컴포넌트를 사용해 이미지 마크업
    - Image 컴포넌트의 이미지의 크기가 ImageCard 컴포넌트의 article의 width에 맞춰지도록 objectFit을 contain으로 설정

---
+++ src/components/ImageCard/ImageCard.tsx
@@ -0,0 +1,38 @@
+import React, { ReactElement } from 'react';
+import { PostInfo } from '../../redux/storeTypes';
+import { ReactComponent as UpIconSVG } from '../../assets/Icon/UpIcon.svg';
+import { ReactComponent as CommentIconSVG } from '../../assets/Icon/commentIcon.svg';
+import { ReactComponent as ViewIconSVG } from '../../assets/Icon/viewIcon.svg';
+import { Image } from '..';
+
+interface ImageCardProps {
+  postInfo: PostInfo;
+}
+
+export default function ImageCard({ postInfo }: ImageCardProps): ReactElement {
+  const thumbnail = postInfo.images[0];
+  const { title, upCount, downCount, commentCount, views } = postInfo;
+  const ALT_TEXT = '사용자 혹은 AI가 작성한 이미지에 대한 구체적인 설명';
+  return (
+    <article>
+      <Image
+        alt={ALT_TEXT}
+        objectFit="contain"
+        src={`https://i.imgur.com/${thumbnail.id}_d.webp?maxwidth=${thumbnail.imageWidth}&shape=thumb&fidelity=high`}
+      />
+      <h3>{title}</h3>
+      <div>
+        <UpIconSVG />
+        <span>{upCount - downCount}</span>
+      </div>
+      <div>
+        <CommentIconSVG />
+        <span>{commentCount}</span>
+      </div>
+      <div>
+        <ViewIconSVG />
+        <span>{views}</span>
+      </div>
+    </article>
+  );
+}
