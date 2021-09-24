28b76fd44110cd50caa2fe452736284faecf14e0
yeongjongkim <tohero21@naver.com>
2021-09-24 15:29:22 +0900

    feat: 버튼 컴포넌트 마크업 & 디자인 완료

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,63 @@
+import React, { ReactElement } from 'react';
+
+// styles
+import { StyledButton } from './Button.styled';
+
+// types
+import { ButtonProps } from './Button.type';
+
+// utils
+import { pxToRem } from '@/util/styleUtils';
+
+export default function Button({
+  alt,
+  backgroundColor,
+  borderRadius,
+  color,
+  fontSize,
+  size,
+  hoverBackgroundColor,
+  hoverColor,
+  img,
+  imageMargin,
+  margin,
+  padding,
+  text,
+  style,
+}: ButtonProps): ReactElement {
+  return (
+    <StyledButton
+      backgroundColor={backgroundColor}
+      borderRadius={borderRadius}
+      color={color}
+      fontSize={fontSize}
+      size={size}
+      hoverBackgroundColor={hoverBackgroundColor}
+      hoverColor={hoverColor}
+      imageMargin={imageMargin}
+      margin={margin}
+      padding={padding}
+      style={style}
+    >
+      {typeof img === 'undefined' ? (
+        ''
+      ) : typeof img === 'string' ? (
+        <img className="img" src={img} alt={alt} />
+      ) : (
+        (() => {
+          const Img = img as React.FC<React.SVGProps<SVGSVGElement>>;
+          return <Img className="img" aria-label={alt} />;
+        })()
+      )}
+      {text}
+    </StyledButton>
+  );
+}
+
+Button.defaultProps = {
+  borderRadius: pxToRem(3),
+  backgroundColor: 'primaryColor',
+  color: 'white',
+  size: 'medium',
+  padding: `0 ${pxToRem(9)} 0 ${pxToRem(9)}`,
+};
