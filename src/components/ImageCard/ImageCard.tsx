55b727aaf2d59592bd14e0a3c2f6defef936744a
HyunJung <HyunJungC.Dev@gmail.com>
2021-09-24 13:34:37 +0900

    feat: auto play 토글 기능 구현
    - 토글 버튼을 누르면 store의 auto play boolean값이 토글되도록 구현 (useDispatch() 이용)
    - store의 auto play값이 바뀌면 그에 따라 비디오 혹은 사진을 렌더링 (useSelector() 이용)

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -1,22 +1,36 @@
 import React, { ReactElement } from 'react';
 import { PostInfo } from '../../redux/storeTypes';
-import { ReactComponent as UpIconSVG } from '../../assets/Icon/UpIcon.svg';
-import { ReactComponent as CommentIconSVG } from '../../assets/Icon/commentIcon.svg';
-import { ReactComponent as ViewIconSVG } from '../../assets/Icon/viewIcon.svg';
+import { ReactComponent as UpIconSVG } from '@/assets/Icon/upIcon.svg';
+import { ReactComponent as CommentIconSVG } from '@/assets/Icon/commentIcon.svg';
+import { ReactComponent as ViewIconSVG } from '@/assets/Icon/viewIcon.svg';
 import { Image, Video } from '..';
 import { StyledImageCard, StyledImageCardFooter } from './ImageCard.styled';
-import { store } from '../../redux/index';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux';
 
 interface ImageCardProps {
   postInfo: PostInfo;
   ImageCardWidth: string | number;
 }
 
+// App.js
+// export default function App(): ReactElement {
+//   const dispatch = useDispatch();
+//   const handleToggle = () => {
+//     dispatch(toggleAutoPlay());
+//   };
+//   return (
+//     <>
+//       <button onClick={handleToggle}>toggle</button>
+//       <ImageCard postInfo={postInfo} ImageCardWidth={240} />
+//     </>
+//   );
+// }
+
 export default function ImageCard({ postInfo, ImageCardWidth }: ImageCardProps): ReactElement {
   const thumbnail = postInfo.images[0];
   const { thumbnailImageId, thumbnailWidth, title, upCount, downCount, commentCount, views } = postInfo;
-  // const isAutoPlay = store.getState().listInfo.autoPlay;
-  const isAutoPlay = false;
+  const isAutoPlay = useSelector((state: RootState) => state.listInfo.autoPlay);
   const ALT_TEXT = '사용자 혹은 AI가 작성한 이미지에 대한 구체적인 설명';
 
   return (
