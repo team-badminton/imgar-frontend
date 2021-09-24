11495d364701849faf384330b5e4f09acce14a75
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-24 19:37:21 +0900

    feat: redux로 화면 크기와 스크롤 위치 상태관리

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,24 @@
-import React, { ReactElement } from 'react';
+import Header from '@/components/Header/Header';
+import useThrottle from '@/hooks/useThrottle';
+import { useTypedDispatch, useTypedSelector } from '@/redux';
+import { displayResize, displayScroll, sizeSelector } from '@/redux/slices/displayReducer';
+import React, { ReactElement, useEffect } from 'react';
 
 export default function App(): ReactElement {
+  const dispatch = useTypedDispatch();
+  const scrollAction = displayScroll();
+  const resizeAction = displayResize();
+  const throttledResize = useThrottle(() => dispatch(resizeAction), 500);
+  useEffect(() => {
+    dispatch(scrollAction);
+    dispatch(resizeAction);
+    window.addEventListener('scroll', () => {
+      dispatch(scrollAction);
+    });
+    window.addEventListener('resize', () => {
+      throttledResize();
+    });
+  }, []);
+
   return <div></div>;
 }
--- src/redux/slices/displayReducer.ts
+++ src/redux/slices/displayReducer.ts
@@ -1,4 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Selector } from 'react-redux';
+import { RootState } from '..';
 import { DisplayInfo } from '../storeTypes';
 
 const initialState: DisplayInfo = {
@@ -23,3 +25,9 @@ export const displaySlice = createSlice({
 
 export const { displayResize, displayScroll } = displaySlice.actions;
 export default displaySlice.reducer;
+
+export const scrollSelector: Selector<RootState, number> = state => state.display.scrollOffset;
+export const sizeSelector: Selector<RootState, Omit<DisplayInfo, 'scrollOffset'>> = state => ({
+  innerHeight: state.display.innerWidth,
+  innerWidth: state.display.innerHeight,
+});
