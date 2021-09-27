952458388aa305eae52e112d0a08534fe2671596
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-27 15:14:24 +0900

    fix: Search 컴포넌트 수정
    - 검색창에서 자동완성 목록의 아이템을 클릭하면 포커스가 풀리면서 Suggest 컴포넌트가 사라지는 문제 해결
    - 검색창에서 링크의 이동이 일어나면 input창이 초기화되고 focus가 해제되도록 변경

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,12 +1,20 @@
 import { useSuggestQuery } from '@/redux/api/v3';
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, { ReactElement, useEffect, useRef, useState } from 'react';
+import { useLocation } from 'react-router';
 import { SearchBar, SuggestList } from '..';
 import { SearchContainer } from './Search.styled';
 
-export default function Search(): ReactElement {
+export default React.memo(function Search(): ReactElement {
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [searchQueryOption, setSearchQueryOption] = useState<'all' | 'tag' | 'user'>('all');
   const [focus, setFocus] = useState<boolean>(false);
+  const location = useLocation();
+  const clearInputRef = useRef<{ clearInput: () => void }>();
+
+  useEffect(() => {
+    setFocus(false);
+    clearInputRef.current.clearInput();
+  }, [location.pathname]);
 
   useEffect(() => {
     if (searchQuery.match(/^#/)) {
@@ -21,8 +29,21 @@ export default function Search(): ReactElement {
   const { data } = useSuggestQuery(searchQuery.replace(/^[@#]/, ''), { skip: !searchQuery && !focus });
 
   return (
-    <SearchContainer>
-      <SearchBar placeholder="Images, #tags, @users oh my!" onQueryChange={setSearchQuery} setFocus={setFocus} />
+    <SearchContainer
+      className="search-container"
+      onFocusCapture={() => setFocus(true)}
+      onBlurCapture={e => {
+        if (!e.nativeEvent.relatedTarget) {
+          setFocus(false);
+        }
+      }}
+    >
+      <SearchBar
+        placeholder="Images, #tags, @users oh my!"
+        onQueryChange={setSearchQuery}
+        setFocus={setFocus}
+        ref={clearInputRef}
+      />
       {!!searchQuery && focus && !!(data?.posts?.length || data?.users?.length || data?.tags?.length) && (
         <SuggestList
           keyword={searchQuery.replace(/^[@#]/, '')}
@@ -33,4 +54,4 @@ export default function Search(): ReactElement {
       )}
     </SearchContainer>
   );
-}
+});
