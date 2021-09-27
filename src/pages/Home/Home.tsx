bc3784c1d92a53bc49c4ae36fc3b2493a40037b1
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-27 15:21:21 +0900

    feat: 테스트를 위해 Home컴포넌트 변경
    - 스크롤 생기도록 설정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 import { useHistory, useLocation, useParams } from 'react-router-dom';
+import { StyledHome } from './Home.styled';
 
 export default function Home(): ReactElement {
-  return <div>Home</div>;
+  return <StyledHome>Home</StyledHome>;
 }
