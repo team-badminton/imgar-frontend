1a2243427be7ccd783a123e69a591afeb35af415
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-28 21:19:25 +0900

    refactor: MainContiner 문제수정
    - customHeader의 사이즈가 달라질 때 상황 추가
    - 기본 헤더를 BasicHeader로 컴포넌트 분리
    - 기본 헤더의 sticky 여부 선택 가능
    - 배경이 어두운 경우 선택 가능
    - 배경 위치 선택가능하게 구현할 것

--- src/components/MainContainer/MainContainer.type.ts
+++ src/components/MainContainer/MainContainer.type.ts
@@ -1,18 +1,18 @@
 export interface HeaderProps {
   children?: React.ReactNode;
   customHeader?: React.ReactNode;
+  customHeaderHeight?: number;
   headerCover?: React.ReactNode;
   headerBackground?: string;
+  darkenBackground?: boolean;
+  sticky?: boolean;
 }
 
 export interface HeaderCoverProps {
   headerBackground?: string;
   hasShadow: boolean;
   headerHeight: number;
-}
-
-export interface HeaderContainerProps {
-  headerHeight: number;
+  darkenBackground?: boolean;
 }
 
 export interface ContainerWrapperProps {
