08983cfb888f20c345f558826b157176f5764232
HyunJung <HyunJungC.Dev@gmail.com>
2021-09-29 17:06:52 +0900

    fix: 태그 스타일 리셋
    - a 태그 기본 스타일 리셋 코드 수정
    - ul 태그 기본 스타일 리셋
    - button 태그 기본 스타일 리셋

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,3 +1,4 @@
+import { pxToRem } from '@/util/styleUtils';
 import React, { ReactElement } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import { Normalize } from 'styled-normalize';
@@ -21,21 +22,28 @@ body *::after {
   box-sizing: inherit;
 }
 
-a {
-  all: unset;
-  cursor: pointer;
-}
-a:link {
-  text-decoration: none;
-}
-a:visited {
-  text-decoration: none;
-}
+a,
+a:hover,
+a:focus,
 a:active {
   text-decoration: none;
+  color: inherit;
 }
-a:hover {
-  text-decoration: none;
+
+ul{
+  margin: 0;
+  padding-left: 0;
+  list-style: none;
+}
+
+button {
+	background: none;
+	color: inherit;
+	border: none;
+	padding: 0;
+	font: inherit;
+	cursor: pointer;
+	outline: inherit;
 }
 `;
 
