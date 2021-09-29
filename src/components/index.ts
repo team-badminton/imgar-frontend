28d237588bae51817727035a3cd531804730ccb9
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-29 21:14:23 +0900

    fix: export 이름을 Image에서 Picture로 변경 누락 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export { default as Button } from './Button/Button';
 export { default as Avatar } from './Avatar/Avatar';
-export { default as Image } from './Picture/Picture';
+export { default as Picture } from './Picture/Picture';
 export { default as PostHeader } from './PostHeader/PostHeader';
 export * from './Picture/Picture.type';
 export { default as Video } from './Video/Video';
