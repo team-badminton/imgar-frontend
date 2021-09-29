818b38a421086452a5935063784706638c38f061
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-29 21:12:00 +0900

    fix: Image 컴포넌트의 이름을 Picture로 변경
    - 사용된 컴포넌트에서도 모두 변경

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
 export { default as Button } from './Button/Button';
 export { default as Avatar } from './Avatar/Avatar';
-export { default as Image } from './Image/Image';
+export { default as Image } from './Picture/Picture';
 export { default as PostHeader } from './PostHeader/PostHeader';
-export * from './Image/Image.type';
+export * from './Picture/Picture.type';
 export { default as Video } from './Video/Video';
 export { default as SearchBar } from './SearchBar/SearchBar';
 export { default as SuggestList } from './SuggestList/SuggestList';
