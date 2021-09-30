6591852641a7e0a4f7caffcc87f005d5707ca8bb
yeongjong310 <tohero21@naver.com>
2021-09-30 21:40:31 +0900

    feat: PostContent에서 이미지/글에 대한  마크업 및 css 완료
    1. 글: 호버시 물결 애니메이션
    2. 이미지 호버시 줌인 커서
    
    todo:
    1. 이미지 호버시 드롭다운 버튼
    2. 드롭다운 메뉴
    3. 태그

--- src/components/PostContents/PostContents.styled.tsx
+++ src/components/PostContents/PostContents.styled.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { StyledPostContentsProps } from './PostContents.type';
+// assets
+import WaveUnderLine from './assets/WaveUnderLine.svg';
 
-export const StyledPostContents = styled.img<StyledPostContentsProps>`
-`
+export const StyledFigure = styled.figure`
+  margin: 0;
+  img,
+  figcaption {
+    margin-bottom: ${({ theme }) => theme.spaceSize.l};
+  }
+  img {
+    cursor: zoom-in;
+  }
+`;
+
+export const StyledFigureCaption = styled.figcaption`
+  color: ${({ theme }) => theme.color.white};
+  display: inline-block;
+  padding: 0 ${({ theme }) => theme.spaceSize.l};
+  a {
+    border-bottom: 1px solid #01b96b;
+    word-break: break-word;
+  }
+  a:hover {
+    background-image: url(${WaveUnderLine});
+    background-position: 0 100%;
+    background-size: auto 36px;
+    background-repeat: repeat;
+    text-decoration: none;
+    border-bottom: none;
+    padding-bottom: 3px;
+  }
+`;
--- src/components/PostContents/PostContents.type.ts
+++ src/components/PostContents/PostContents.type.ts
@@ -1,7 +1,11 @@
 import { CSSProperties } from 'react';
+import { ImageInfo, TagInfo } from '@/redux/storeTypes';
 
-export interface StyledPostContentsProps {
-}
+export interface StyledPostContentsProps {}
 
 export interface PostContentsProps extends StyledPostContentsProps {
+  /** Gallery api를 통해 받아온 응답 json에서 images prop 배열입니다. image를 불러오기 위한 id와 description을 사용합니다. */
+  images: ImageInfo[];
+  /** Gallery api를 통해 받아온 응답 json에서 tags prop 배열입니다. */
+  tags: TagInfo[];
 }
