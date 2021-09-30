87d0818993fc222db88c8a9c4036500a6899c57a
yeongjong310 <tohero21@naver.com>
2021-09-30 21:41:18 +0900

    feat: 글 컨텐츠가 URI를 포함하는지에 따른 스토리 추가

--- src/components/PostContents/PostContents.stories.tsx
+++ src/components/PostContents/PostContents.stories.tsx
@@ -22,7 +22,56 @@ Default.args = {
   images: [
     {
       bandWidth: 75155070963,
-      description: 'https://www.instagram.com/mk_ays/\n\nhttps://www.instagram.com/p/CBTjzNApIb6/?igshid=7cvtuawzabvw',
+      description: "I'm on the next level",
+      hasSound: false,
+      id: 'Ys263e0',
+      imageHeight: 1080,
+      imageWidth: 1080,
+      type: 'image/png',
+    },
+  ],
+  tags: [
+    {
+      name: 'kyoshi warriors',
+      displayName: 'kyoshi warriors',
+      postCount: 1,
+      backgroundImageId: '1',
+      description: '1',
+    },
+  ],
+};
+
+export const NonUriTextWithURI = Template.bind({});
+NonUriTextWithURI.args = {
+  images: [
+    {
+      bandWidth: 75155070963,
+      description: 'Non URI TEXT\n\nhttps://www.instagram.com/p/CBTjzNApIb6/?igshid=7cvtuawzabvw',
+      hasSound: false,
+      id: 'Ys263e0',
+      imageHeight: 1080,
+      imageWidth: 1080,
+      type: 'image/png',
+    },
+  ],
+  tags: [
+    {
+      name: 'kyoshi warriors',
+      displayName: 'kyoshi warriors',
+      postCount: 1,
+      backgroundImageId: '1',
+      description: '1',
+    },
+  ],
+};
+
+export const NonUriTextBetweenURIs = Template.bind({});
+NonUriTextBetweenURIs.args = {
+  images: [
+    {
+      bandWidth: 75155070963,
+      description:
+        'https://www.instagram.com/mk_ays/\n\nNon URI TEXT\n\nhttps://www.instagram.com/p/CBTjzNApIb6/?igshid=7cvtuawzabvw',
       hasSound: false,
       id: 'Ys263e0',
       imageHeight: 1080,
