01bb1e6ba83504a8be3e31efa810bbc705b26d5b
yeongjong310 <tohero21@naver.com>
2021-09-30 21:21:22 +0900

    feat: PostContent의 기본 스토리 추가

--- src/components/PostContents/PostContents.stories.tsx
+++ src/components/PostContents/PostContents.stories.tsx
@@ -9,7 +9,7 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: `**PostContents** 컴포넌트는 ...`,
+        component: `**PostContents** 컴포넌트는 이미지/글/tag를 포함한 POST 내용을 보여줍니다. 글의 경우 URI 형식에 한해 밑줄이 그어지며, hover시 애니메이션이 동작합니다.`,
       },
     },
   },
@@ -18,3 +18,25 @@ export default {
 const Template: ComponentStory<typeof PostContents> = args => <PostContents {...args} />;
 
 export const Default = Template.bind({});
+Default.args = {
+  images: [
+    {
+      bandWidth: 75155070963,
+      description: 'https://www.instagram.com/mk_ays/\n\nhttps://www.instagram.com/p/CBTjzNApIb6/?igshid=7cvtuawzabvw',
+      hasSound: false,
+      id: 'Ys263e0',
+      imageHeight: 1080,
+      imageWidth: 1080,
+      type: 'image/png',
+    },
+  ],
+  tags: [
+    {
+      name: 'kyoshi warriors',
+      displayName: 'kyoshi warriors',
+      postCount: 1,
+      backgroundImageId: '1',
+      description: '1',
+    },
+  ],
+};
