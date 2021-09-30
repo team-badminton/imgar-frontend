85315845fe1032b9c419fd4de8b28e9131cd8680
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-30 19:59:55 +0900

    fix: pathname의 끝부분에 slash제거
    - url-regexp의 기능으로 url끝에 /가 추가로 붙으면 모두 제거해서 redirection

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,11 +2,12 @@ import useThrottle from '@/hooks/useThrottle';
 import { Gallery, Home, Profile, SearchResult, TagDetail } from '@/pages';
 import { useTypedDispatch } from '@/redux';
 import { displayResize, displayScroll } from '@/redux/slices/displayReducer';
-import React, { ReactElement, useEffect, useRef } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { ReactElement, useEffect } from 'react';
+import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
 
 export default function App(): ReactElement {
   const dispatch = useTypedDispatch();
+  const { pathname } = useLocation();
   const throttledResize = useThrottle((w: number, h: number) => dispatch(displayResize({ width: w, height: h })), 500);
   useEffect(() => {
     dispatch(displayScroll(window.scrollY));
@@ -22,9 +23,10 @@ export default function App(): ReactElement {
   return (
     <>
       <Switch>
+        <Redirect exact from="/:url*(/+)/" to={pathname.slice(0, -1)} />
         <Route path="/" exact component={Home} />
         <Route path="/gallery/:id" exact component={Gallery} />
-        <Route path="/user/:username" exact component={Profile} />
+        <Route path="/user/:username" component={Profile} />
         <Route path="/t/:tagname" exact component={TagDetail} />
         <Route path="/search" component={SearchResult} />
         <Route path="/" component={Home} />
