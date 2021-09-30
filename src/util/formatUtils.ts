9d39bcf3be730536dfa226204435e7999bf3fe62
yeongjong310 <tohero21@naver.com>
2021-09-30 21:22:40 +0900

    feat: url 체크 함수 util에서 제거 후 컴포넌트 내부로 이동

--- src/util/formatUtils.ts
+++ src/util/formatUtils.ts
@@ -1,15 +1,3 @@
 export function formattedNumber(num: number | string) {
   return num.toLocaleString('en-US');
 }
-
-export function isValidUrlFormat(str: string) {
-  let url;
-
-  try {
-    url = new URL(str);
-  } catch {
-    return false;
-  }
-
-  return url.protocol === 'http:' || url.protocol === 'https:';
-}
