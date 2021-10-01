1d49e9c02e96a9397160adf817bc49dacc94db06
kimjeongwon <kimjeongwonn@gmail.com>
2021-10-01 21:21:59 +0900

    feat: Tab 라우팅 정의중
    - Profile의 탭으로 이루어진 페이지 라우팅

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,32 +1,53 @@
 import MainContainer from '@/components/MainContainer/MainContainer';
-import UserInfo from '@/components/UserInfo/UserInfo';
-import { useAccountQuery } from '@/redux/api/v3';
-import React, { ReactElement } from 'react';
-import { useHistory, useLocation, useParams } from 'react-router-dom';
+import TabNavigation from '@/components/TabNavigation/TabNavigation';
+import { useAccountQuery } from '@/redux/api';
+import React, { ReactElement, useRef } from 'react';
+import { Route, Switch, useLocation, useParams, useRouteMatch, RouteComponentProps } from 'react-router-dom';
+import styled from 'styled-components';
+import ProfileCover from './ProfileCover';
 
-export default function Profile(): ReactElement {
-  const location = useLocation();
-  const history = useHistory();
+const ProfileWrapper = React.memo(styled.div`
+  padding-top: 75px;
+`);
+
+const Favorites = () => <div style={{ height: '150vh' }}>FAVORITES</div>;
+const Comments = () => <div style={{ height: '150vh' }}>Comments</div>;
+const About = () => <div style={{ height: '150vh' }}>About</div>;
+const Posts = () => <div style={{ height: '150vh' }}>Posts</div>;
+
+const tabs = [
+  { label: 'Posts', path: 'posts' },
+  { label: 'Favorites', path: 'favorites' },
+  { label: 'Comments', path: 'comments' },
+  { label: 'About', path: 'about' },
+];
+
+export default function Profile({ location, match }: RouteComponentProps): ReactElement {
+  const { pathname } = location;
   const { username } = useParams<{ username: string }>();
-  const { isSuccess, data } = useAccountQuery(username);
-  console.log(data?.notoriety);
-  if (isSuccess) {
-    return (
-      <MainContainer
-        headerBackground={data?.coverUrl}
-        headerCover={<UserInfo username={data.name} points={data.points} notoriety={data.notoriety} />}
-        customHeader={<div style={{ height: 120 }}>높이가 커져라</div>}
-        darkenBackground
-        customHeaderHeight={120}
-        // sticky
-      >
-        <div style={{ height: '300vh' }}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus est, perferendis quidem nesciunt sunt aut
-          placeat dolor, adipisci esse corporis accusantium quaerat! Quasi, dolore voluptatem maxime nihil commodi
-          reiciendis inventore!
-        </div>
-      </MainContainer>
-    );
-  }
-  return <div>Loading</div>;
+  const { data } = useAccountQuery(username);
+  const customHederHeight = pathname.includes('/favorites') ? 200 : 120;
+  console.log('customHederHeight: ' + customHederHeight);
+
+  return (
+    <MainContainer
+      headerBackground={data?.coverUrl}
+      headerCover={
+        <ProfileCover username={data?.name} points={data?.points} notoriety={data?.notoriety}>
+          <TabNavigation tabs={tabs} />
+        </ProfileCover>
+      }
+      darkenBackground
+      customHeaderHeight={customHederHeight}
+    >
+      <ProfileWrapper>
+        <Switch>
+          <Route path={match.url + `/favorites`} exact component={Favorites} />
+          <Route path={match.url + `/comments`} exact component={Comments} />
+          <Route path={match.url + `/about`} exact component={About} />
+          <Route path={match.url} component={Posts} />
+        </Switch>
+      </ProfileWrapper>
+    </MainContainer>
+  );
 }
