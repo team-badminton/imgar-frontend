420a947bd0a7d3ba2ed8b2662dd5e3fb969bfcf8
kimjeongwon <kimjeongwonn@gmail.com>
2021-10-03 17:34:41 +0900

    feat: Profile 페이지에서 MainContainer에 Cover로 넣을 ProfileCover 컴포넌트 설계
    - UserInfo와 TabNavigation을 사용

---
+++ src/pages/Profile/Profile.styled.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ProfileCoverContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+
+  :root {
+    background-color: yellowgreen;
+  }
+`;
