b212706e1ede7b4a271783911cd64f1d929f2013
HyunJung <HyunJungC.Dev@gmail.com>
2021-10-04 15:27:12 +0900

    fix: ImageCard storybook 설명 추가
    - ImageCard storybook 설명 추가

--- src/components/ImageCard/ImageCard.stories.tsx
+++ src/components/ImageCard/ImageCard.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import ImageCard from './ImageCard';
+import { IMAGECARD_WIDTH_PX } from './ImageCard.styled';
 
 export default {
   title: 'Components/ImageCard',
@@ -9,7 +10,14 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: `ImageCard 컴포넌트는 store의 listInfo.autoPlay의 Boolean 값과 안에 들어가는 이미지가 사진인지 동영상인지, 동영상이라면 소리가 있는지 없는지에 따라 다른 레이아웃을 가지는 컴포넌트 입니다.`,
+        component: `ImageCard 컴포넌트는 게시글의 썸네일 이미지(사진 혹은 동영상)와 제목, 그리고 추천수,댓글수, 조회수를 보여주는 컴포넌트입니다. \n
+- 게시글의 썸네일 이미지는 사진일수도 동영상일 수도 있습니다.
+- 게시글의 썸네일 이미지는 images 배열의 첫번째 요소입니다. images[0].type에 따라 사진인지 동영상인지 여부를 판별합니다.
+- 리덕스에서 관리하는 listInfo.autoPlay의 Boolean 값에 따라 썸네일 이미지의 자동 재생 여부가 결정됩니다.
+  - 썸네일 이미지가 사진인 경우, autoPlay의 Boolean 값에 영향받지 않습니다.
+  - 썸네일 이미지가 동영상인 경우, autoPlay가 true면 동영상이 렌더링되고, false면 사진이 렌더링 됩니다.
+- autoPlay가 false일 때,  images[0].hasSound에 따라 동영상의 경우 소리가 있는지 없는 지 여부를 판별하여 표시합니다. 
+        `,
       },
     },
   },
@@ -20,7 +28,7 @@ const Template: ComponentStory<typeof ImageCard> = args => <ImageCard {...args} 
 export const autoplayTrueAndVideo = Template.bind({});
 autoplayTrueAndVideo.args = {
   isAutoPlay: true,
-  imageCardWidth: 240,
+  imageCardWidth: IMAGECARD_WIDTH_PX,
   postInfo: {
     title: 'The Black Purral',
     thumbnailImageId: 'GGSA0v9',
