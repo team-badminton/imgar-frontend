006fe30e19b3651edaa37564a7883bac3acce6f7
yeongjongkim <tohero21@naver.com>
2021-10-06 22:51:49 +0900

    feat: max-width 제거

--- src/components/Picture/Picture.styled.tsx
+++ src/components/Picture/Picture.styled.tsx
@@ -4,7 +4,6 @@ import { StyledImageProps } from './Picture.type';
 
 export const StyledImage = styled.img<StyledImageProps>`
   ${({ imageWidth }) => imageWidth && `width: ${widthConvertor(imageWidth)};`}
-  max-width: 100%;
   object-fit: ${({ objectFit }) => objectFit};
   height: ${({ isCircle, imageWidth }) => (isCircle ? imageWidth : '')};
   border-radius: ${({ isCircle }) => (isCircle ? '50%' : '')};
