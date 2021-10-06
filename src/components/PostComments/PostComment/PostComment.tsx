24b25862f032b1f005a579dec2f05830b51b65ea
yeongjongkim <tohero21@naver.com>
2021-10-06 15:35:20 +0900

    feat: PostComment 단일 댓글에 대한 마크업 및 로직 구현
    - 대댓글 존재시 확장가능하도록 구현됨
    
    남은 과제
    - 스타일링
    - 접근성 개선
    - expanded all

---
+++ src/components/PostComments/PostComment/PostComment.tsx
@@ -0,0 +1,62 @@
+import React, { ReactElement, useState } from 'react';
+
+// components
+import { Avatar, Vote } from '@/components';
+
+// types
+import { PostCommentInfo } from '@/redux/storeTypes';
+
+// styles
+import {
+  PostCommentContainer,
+  StyledButton,
+  ChildrenCommentsContainer,
+  VoteContainer,
+  Comment,
+} from './PostComment.styled';
+
+export default function PostComment({
+  id,
+  author,
+  childrenComments,
+  comment,
+  dateTime,
+  downCount,
+  upCount,
+}: Omit<PostCommentInfo, 'thumbnailImageId' | 'parentCommentId' | 'postId'>): ReactElement {
+  const [isExpendedChildComments, setIsExpendedChildComments] = useState(false);
+
+  return (
+    <PostCommentContainer>
+      <Avatar username={author} size="small" infoRows={1} metaInfos={{ time: dateTime, platform: 'Iphone' }} />
+      <Comment>{comment}</Comment>
+      <VoteContainer>
+        <Vote size="small" direction="row" count={upCount - downCount} />
+        {childrenComments?.length !== 0 && (
+          <StyledButton
+            onClick={() => setIsExpendedChildComments(!isExpendedChildComments)}
+            backgroundColor="transparent"
+            size="custom"
+            text={`+ ${childrenComments.length} reply`}
+          />
+        )}
+      </VoteContainer>
+      {childrenComments && isExpendedChildComments && (
+        <ChildrenCommentsContainer>
+          {childrenComments.map(({ id, author, childrenComments, comment, dateTime, downCount, upCount }) => (
+            <PostComment
+              key={id}
+              id={id}
+              author={author}
+              childrenComments={childrenComments}
+              comment={comment}
+              dateTime={dateTime}
+              downCount={downCount}
+              upCount={upCount}
+            />
+          ))}
+        </ChildrenCommentsContainer>
+      )}
+    </PostCommentContainer>
+  );
+}
