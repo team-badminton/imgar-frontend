df55be9cb329190e23fbcd6c918f4ab7f458da37
kimjeongwon <kimjeongwonn@gmail.com>
2021-10-06 20:24:10 +0900

    feat: MainContainer에서 width 수정 가능, galleryWidth 리덕스에서 관리
    - MainContainer에 containerWidth prop을 변경해 컨테이너 내부의 너비 지정가능
    - galleryWidth를 리덕스에서 계산하여 사용할 수 있음
    - 추가로 gallery의 column갯수도 리덕스에서 별도로 관리
    - innerWidth를 직접 리덕스를 통해 가져오지 않게하여 리렌더링 최소화

--- src/components/MainContainer/MainContainer.tsx
+++ src/components/MainContainer/MainContainer.tsx
@@ -1,7 +1,12 @@
 import React, { ReactElement, useLayoutEffect, useRef, useState } from 'react';
-import { createGlobalStyle } from 'styled-components';
 import BasicHeader from './BasicHeader/BasicHeader';
-import { ContainerWrapper, HeaderContainer, HeaderCover, MainSection } from './MainContainer.styled';
+import {
+  ChangeGlobalBackground,
+  ContainerWrapper,
+  HeaderContainer,
+  HeaderCover,
+  MainSection,
+} from './MainContainer.styled';
 import { HeaderProps } from './MainContainer.type';
 
 export default function MainContainer({
@@ -15,6 +20,7 @@ export default function MainContainer({
   backgroundColor,
   headerCoverPosition,
   noOffset,
+  containerWidth,
 }: HeaderProps): ReactElement {
   const headerCoverRef = useRef<HTMLDivElement>(null);
   const headerContainerRef = useRef<HTMLDivElement>(null);
@@ -24,11 +30,13 @@ export default function MainContainer({
   const [showCustomHeader, setShowCustomHeader] = useState<boolean>(false);
   const [coverHeight, setCoverHeight] = useState<number>(0);
 
-  const ChangeGlobalBackground = createGlobalStyle`
-  body {
-    background-color: ${({ theme }) => theme.color[backgroundColor]};
-  }
-  `;
+  useLayoutEffect(() => {
+    Array.from(headerCoverRef.current.children).forEach((child: HTMLElement) => {
+      child.style.width = `${containerWidth}px`;
+    });
+    mainSectionRef.current.style.width = `${containerWidth}px`;
+    // mainSectionRef
+  }, [containerWidth]);
 
   useLayoutEffect(() => {
     const computedCoverHeight = headerCoverRef.current?.scrollHeight;
@@ -100,8 +108,9 @@ export default function MainContainer({
 
   return (
     <>
-      {backgroundColor ? <ChangeGlobalBackground /> : null}
+      {backgroundColor ? <ChangeGlobalBackground backgroundColor={backgroundColor} /> : null}
       <ContainerWrapper gradient={!headerCover}>
+        <HeaderContainer ref={headerContainerRef}>{showCustomHeader ? customHeader : <BasicHeader />}</HeaderContainer>
         <HeaderCover
           headerBackground={!!headerCover && headerBackground}
           darkenBackground={darkenBackground}
@@ -112,7 +121,6 @@ export default function MainContainer({
         >
           {headerCover ?? <div style={{ height: BASIC_HEADER_HEIGHT }} />}
         </HeaderCover>
-        <HeaderContainer ref={headerContainerRef}>{showCustomHeader ? customHeader : <BasicHeader />}</HeaderContainer>
         <MainSection ref={mainSectionRef} coverHeight={coverHeight}>
           {children}
         </MainSection>
