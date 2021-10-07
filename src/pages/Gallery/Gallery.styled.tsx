63005fdf7ff2a063dbb70c205dec994bb2d35849
yeongjongkim <tohero21@naver.com>
2021-10-08 04:34:01 +0900

    feat: 갤러리에 컴포넌트 조합(PostHeader, PostFigure, PostComments)

---
+++ src/pages/Gallery/Gallery.styled.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const ContentContainer = styled.div`
+  margin: 0 ${({ theme }) => theme.spaceSize.l};
+  padding: 0 ${({ theme }) => theme.spaceSize.l};
+  ${({ theme }) => theme.spaceSize.l}; ;
+`;
