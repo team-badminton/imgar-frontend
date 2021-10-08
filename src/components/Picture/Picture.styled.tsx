97cc6512861a448a34cb21473445df9ef7801008
kimjeongwon <kimjeongwonn@gmail.com>
2021-10-08 11:51:25 +0900

    feat: Picture컴포넌트에서 height 명시 추가

--- src/components/Picture/Picture.styled.tsx
+++ src/components/Picture/Picture.styled.tsx
@@ -1,4 +1,4 @@
-import { widthConvertor } from '@/util/styleUtils';
+import { pxToRem, widthConvertor } from '@/util/styleUtils';
 import styled from 'styled-components';
 import { StyledImageProps } from './Picture.type';
 
@@ -6,6 +6,6 @@ export const StyledImage = styled.img<StyledImageProps>`
   ${({ imageWidth }) => imageWidth && `width: ${widthConvertor(imageWidth)};`}
   max-width: 100%;
   object-fit: ${({ objectFit }) => objectFit};
-  height: ${({ isCircle, imageWidth }) => (isCircle ? imageWidth : '')};
+  height: ${({ isCircle, imageWidth, imageHeight }) => pxToRem(imageHeight) ?? (isCircle ? pxToRem(imageWidth) : '')};
   border-radius: ${({ isCircle }) => (isCircle ? '50%' : '')};
 `;
