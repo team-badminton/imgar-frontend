a490099aac19c2db99da7b706842c2c468959cf0
HyunJung <HyunJungC.Dev@gmail.com>
2021-10-08 22:30:04 +0900

    fix: SelectBox 컴포넌트로 MasonryGalleryFields 컴포넌트 수정
    - DropDownList 컴포넌트 내에서의 헤더 변경과 밖에서 dispatch()로 일어나는 헤더 변경 사이의 리렌더링 문제를 해결하기 위해 헤더를 변경하는 부분을 하나로 통일
    - 헤더는 항상 redux의 상태로 가져오고 헤더를 변경하고자 할 때는 redux의 상태를 변경한다.

--- src/components/MasonryGalleryFields/MasonryGalleryFields.tsx
+++ src/components/MasonryGalleryFields/MasonryGalleryFields.tsx
@@ -1,8 +1,9 @@
-import React, { ReactElement, useEffect, useMemo } from 'react';
+import React, { ReactElement, useMemo, useState } from 'react';
 import { useTypedSelector, useTypedDispatch } from '@/redux';
-import { DropDownList } from '@/components';
+import { SelectBox, SelectBoxHeader, SelectBoxList } from '@/components';
 import { setCategory, setSortOption, setWindowOption } from '@/redux/slices/listInfoReducer';
 import { GalleryQuery } from '@/redux/api/types/queries';
+import { createRandomHash } from '@/util/formatUtils';
 
 const galleryQueryKeyToAllUpperCase = (
   galleryQueryKey: GalleryQuery['section'] | GalleryQuery['sort'] | GalleryQuery['window'],
@@ -22,48 +23,53 @@ const allUpperCaseToGalleryQueryKey = (
     | GalleryQuery['window'][];
 };
 
+const categoryList: GalleryQuery['section'][] = ['mostViral', 'userSubmitted', 'highestScoring'];
+const sortOptionList: { [key in GalleryQuery['section']]: GalleryQuery['sort'][] | GalleryQuery['window'][] } = {
+  mostViral: ['newest', 'popular', 'best', 'random'],
+  userSubmitted: ['popular', 'rising', 'newest'],
+  highestScoring: ['day', 'week', 'month', 'year', 'all'],
+};
+
 export default function MasonryGalleryFields(): ReactElement {
-  // MASONRY GALLERY 헤더의 최소 너비 설정
-  const categoryList: GalleryQuery['section'][] = ['mostViral', 'userSubmitted', 'highestScoring'];
-  const sortOptionList: { [key in GalleryQuery['section']]: GalleryQuery['sort'][] | GalleryQuery['window'][] } = {
-    mostViral: ['newest', 'popular', 'best', 'random'],
-    userSubmitted: ['popular', 'rising', 'newest'],
-    highestScoring: ['day', 'week', 'month', 'year', 'all'],
-  };
-
-  // 카테고리 리스트를 두번 클릭해야 적용되는 문제 해결?
-  const categoryListReactElement = useMemo(
-    () =>
-      categoryList
-        .map(category => galleryQueryKeyToAllUpperCase(category))
-        .map((item, index) => <span key={index}>{item}</span>),
-    [],
-  );
-
   const dispatch = useTypedDispatch();
   const category = useTypedSelector(state => state.listInfo.category);
+  const sortOption = useTypedSelector(state => state.listInfo.sortOption);
+  const windowOption = useTypedSelector(state => state.listInfo.windowOption);
 
-  const handleSetCategory =
-    ($selectedLi: HTMLLIElement, $selectedChild: ReactElement) =>
-    (e: React.MouseEvent<HTMLUListElement, MouseEvent>) => {
-      const selectedCategory = allUpperCaseToGalleryQueryKey($selectedChild.props.children);
-      const selectedSort = allUpperCaseToGalleryQueryKey(
-        sortOptionList[selectedCategory as GalleryQuery['section']][0],
-      );
-      dispatch(setCategory(selectedCategory as GalleryQuery['section']));
-      dispatch(setSortOption(selectedSort as GalleryQuery['sort']));
-    };
+  const displayedCategory = galleryQueryKeyToAllUpperCase(category);
+  const displayedSortOption = galleryQueryKeyToAllUpperCase(sortOption);
+  const displayedWindowOption = galleryQueryKeyToAllUpperCase(windowOption);
 
-  const handleSetSortOrWindowOption =
-    ($selectedLi: HTMLLIElement, $selectedChild: ReactElement) =>
-    (e: React.MouseEvent<HTMLUListElement, MouseEvent>) => {
-      const newSortOrWindowOption = allUpperCaseToGalleryQueryKey($selectedChild.props.children);
-      if (category === 'highestScoring') {
-        dispatch(setWindowOption(newSortOrWindowOption as GalleryQuery['window']));
-      } else {
-        dispatch(setSortOption(newSortOrWindowOption as GalleryQuery['sort']));
-      }
-    };
+  const handleSetCategory = (e: React.MouseEvent<HTMLUListElement, MouseEvent>) => {
+    const $target = e.target as HTMLElement;
+    const $selectedLi = $target.closest('.listItem') as HTMLLIElement;
+    if (!$selectedLi) {
+      return;
+    }
+
+    const selectedCategory = allUpperCaseToGalleryQueryKey($selectedLi.children[0].children[0].textContent);
+    const selectedSort = allUpperCaseToGalleryQueryKey(sortOptionList[selectedCategory as GalleryQuery['section']][0]);
+    dispatch(setCategory(selectedCategory as GalleryQuery['section']));
+    dispatch(setSortOption(selectedSort as GalleryQuery['sort']));
+  };
+
+  const handleSetSortOrWindowOption = (e: React.MouseEvent<HTMLUListElement, MouseEvent>) => {
+    const $target = e.target as HTMLElement;
+    const $selectedLi = $target.closest('.listItem') as HTMLLIElement;
+    if (!$selectedLi) {
+      return;
+    }
+
+    const newSortOrWindowOption = allUpperCaseToGalleryQueryKey($selectedLi.children[0].children[0].textContent);
+    if (category === 'highestScoring') {
+      dispatch(setWindowOption(newSortOrWindowOption as GalleryQuery['window']));
+    } else {
+      dispatch(setSortOption(newSortOrWindowOption as GalleryQuery['sort']));
+    }
+  };
+
+  const [isShowCategory, setIsShowCategory] = useState<boolean>(false);
+  const [isShowSortOrWindowOption, setIsShowSortOrWindowOption] = useState<boolean>(false);
 
   return (
     <>
@@ -74,26 +80,34 @@ export default function MasonryGalleryFields(): ReactElement {
           width: 100%;
         `}
       >
-        <DropDownList
-          handlerOption={{
-            useType: 'selectBox',
-            handleDropDownList: handleSetCategory,
-          }}
-        >
-          {categoryListReactElement}
-        </DropDownList>
-        <DropDownList
-          handlerOption={{
-            useType: 'selectBox',
-            handleDropDownList: handleSetSortOrWindowOption,
-          }}
-        >
-          {sortOptionList[category]
-            .map(sortOption => galleryQueryKeyToAllUpperCase(sortOption))
-            .map((sortOption, index) => (
-              <span key={index}>{sortOption}</span>
-            ))}
-        </DropDownList>
+        <SelectBox>
+          <SelectBoxHeader onClickHandler={() => setIsShowCategory(!isShowCategory)}>
+            {displayedCategory}
+          </SelectBoxHeader>
+          <SelectBoxList onClickHandler={handleSetCategory} isShow={isShowCategory}>
+            {categoryList
+              .map(category => galleryQueryKeyToAllUpperCase(category))
+              .map(item => (
+                <button key={createRandomHash()}>
+                  <span>{item}</span>
+                </button>
+              ))}
+          </SelectBoxList>
+        </SelectBox>
+        <SelectBox>
+          <SelectBoxHeader onClickHandler={() => setIsShowSortOrWindowOption(!isShowSortOrWindowOption)}>
+            {category !== 'highestScoring' ? displayedSortOption : displayedWindowOption}
+          </SelectBoxHeader>
+          <SelectBoxList onClickHandler={handleSetSortOrWindowOption} isShow={isShowSortOrWindowOption}>
+            {sortOptionList[category]
+              .map(sortOption => galleryQueryKeyToAllUpperCase(sortOption))
+              .map(sortOption => (
+                <button key={createRandomHash()}>
+                  <span>{sortOption}</span>
+                </button>
+              ))}
+          </SelectBoxList>
+        </SelectBox>
       </div>
     </>
   );
