b7edb7a853f3cd56f8fba71167cd24ae087020b3
kimjeongwon <kimjeongwonn@gmail.com>
2021-10-09 16:41:02 +0900

    fix: FolderList Button 추가

--- src/pages/Profile/Favorite/ProfileFavoriteFolders.tsx
+++ src/pages/Profile/Favorite/ProfileFavoriteFolders.tsx
@@ -2,10 +2,13 @@ import { ContainerWidthContext } from '@/components/MainContainer/MainContainer'
 import useThrottle from '@/hooks/useThrottle';
 import { useAccountFoldersQuery } from '@/redux/api';
 import { FolderInfo } from '@/redux/storeTypes';
+import { pxToRem } from '@/util/styleUtils';
 import React, { ReactElement, useContext, useEffect, useRef, useState } from 'react';
 import { useRouteMatch } from 'react-router-dom';
 import { FolderIcon, FolderLink, FolderList, FolderName, FoldersWrapper, ScrollButton } from './ProfileFavorite.styled';
 import { ProfileFavoriteFoldersProps } from './ProfileFavorite.type';
+import { ReactComponent as LeftSvgIcon } from '@/assets/Icon/leftIcon.svg';
+import { ReactComponent as RightSvgIcon } from '@/assets/Icon/rightIcon.svg';
 
 function FolderItem({ id, name, allItem }: Partial<FolderInfo> & { allItem?: boolean }): ReactElement {
   const { url } = useRouteMatch();
@@ -18,7 +21,11 @@ function FolderItem({ id, name, allItem }: Partial<FolderInfo> & { allItem?: boo
     : url + '/favorites';
 
   return (
-    <li>
+    <li
+      css={`
+        margin-right: ${pxToRem(4)};
+      `}
+    >
       <FolderLink
         to={to}
         activeClassName="isActive"
@@ -38,8 +45,7 @@ function FolderItem({ id, name, allItem }: Partial<FolderInfo> & { allItem?: boo
 }
 
 export default function ProfileFavoriteFolders({ username }: ProfileFavoriteFoldersProps): ReactElement {
-  const containerWidth = useContext(ContainerWidthContext);
-  const { data, isSuccess } = useAccountFoldersQuery(username);
+  const { data } = useAccountFoldersQuery(username);
   const listRef = useRef<HTMLUListElement>(null);
   const [hasScroll, setHasScroll] = useState<boolean>(false);
   const [scrolled, setScrolled] = useState<boolean>(false);
@@ -61,27 +67,32 @@ export default function ProfileFavoriteFolders({ username }: ProfileFavoriteFold
 
   useEffect(() => {
     handleResize();
-    listRef.current.addEventListener('resize', handleResize);
+    window.addEventListener('resize', handleResize);
     listRef.current.addEventListener('scroll', handleScroll);
 
     return () => {
-      listRef.current.removeEventListener('resize', handleResize);
+      window.removeEventListener('resize', handleResize);
       listRef.current.removeEventListener('scroll', handleScroll);
     };
   }, [data]);
 
-  console.log(hasScroll);
-  console.log(scrolled);
-
   return (
-    <FoldersWrapper containerWidth={containerWidth}>
+    <FoldersWrapper>
+      {scrolled ? (
+        <ScrollButton $direction="prev">
+          <LeftSvgIcon />
+        </ScrollButton>
+      ) : null}
+      {hasScroll ? (
+        <ScrollButton $direction="next">
+          <RightSvgIcon />
+        </ScrollButton>
+      ) : null}
       <FolderList ref={listRef}>
-        {scrolled ? <ScrollButton direction="prev" /> : null}
         {data && [
           <FolderItem name="All Favorites" key={0} allItem />,
           data.map(folder => <FolderItem key={folder.id} {...folder} />),
         ]}
-        {hasScroll ? <ScrollButton direction="next" /> : null}
       </FolderList>
     </FoldersWrapper>
   );
