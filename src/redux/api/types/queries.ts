18c33843e1f585f8ddd7dde89d6a33516acc4399
kimjeongwon <kimjeongwonn@gmail.com>
2021-10-10 19:55:52 +0900

    fix: 사용자 Posts목록을 가져오는 쿼리수정
    - sort 추가
    - API V1으로 노멀라이즈

--- src/redux/api/types/queries.ts
+++ src/redux/api/types/queries.ts
@@ -29,6 +29,7 @@ export interface AccountCommentQuery {
 export interface AccountPostQuery {
   username: string;
   page?: number;
+  sort: 'oldest' | 'newest' | 'best';
 }
 export interface accountFavoriteFolderQuery {
   username: string;
