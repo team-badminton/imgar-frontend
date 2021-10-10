008cc87626482d6b58402152fd939e5d1db12d57
kimjeongwon <kimjeongwonn@gmail.com>
2021-10-10 18:53:05 +0900

    feat: ProfileFavoriteHeader컴포넌트 정의
    - Favorite폴더명과 Gallery의 설정을 바꿀 수 있음

---
+++ src/pages/Profile/Favorite/ProfileFavoriteHeader.tsx
@@ -0,0 +1,63 @@
+import { DropDownList, MasonryGalleryOptions } from '@/components';
+import { useAccountFoldersQuery } from '@/redux/api';
+import React, { ReactElement } from 'react';
+import { useRouteMatch } from 'react-router';
+import { ProfileSort } from './ProfileFavorite';
+import { ProfileFavoriteHeaderContainer, ProfileFavoriteHeaderTitle } from './ProfileFavorite.styled';
+
+function ProfileFavoriteFields({ setSort }: { setSort: (s: ProfileSort) => void }): ReactElement {
+  const sortList = ['oldest', 'newest'];
+
+  const handleSetSort = (_: never, item: ReactElement) => () => {
+    setSort(item.props.children.toLowerCase());
+  };
+
+  return (
+    <>
+      <div
+        css={`
+          display: flex;
+          justify-content: space-between;
+          width: 100%;
+        `}
+      >
+        <DropDownList
+          handlerOption={{
+            useType: 'selectBox',
+            handleDropDownList: handleSetSort,
+          }}
+        >
+          {sortList
+            .map(sortOption => sortOption)
+            .map((sortOption, index) => (
+              <span key={index}>{sortOption.toUpperCase()}</span>
+            ))}
+        </DropDownList>
+      </div>
+    </>
+  );
+}
+
+export default function ProfileFavoriteHeader({ setSort }: { setSort: (s: ProfileSort) => void }): ReactElement {
+  const match = useRouteMatch<{ username: string; folderId: string }>([
+    '/user/:username/favorites/folder/:folderId',
+    '/user/:username/favorites',
+  ]);
+  const { data: folders } = useAccountFoldersQuery(match.params.username);
+
+  const folderName = folders?.find(folder => folder.id + '' === match.params.folderId)?.name;
+
+  return (
+    <ProfileFavoriteHeaderContainer>
+      <ProfileFavoriteHeaderTitle>{folders && (folderName ?? 'All Favorites')}</ProfileFavoriteHeaderTitle>
+      <div
+        css={`
+          display: flex;
+        `}
+      >
+        <ProfileFavoriteFields setSort={setSort} />
+        <MasonryGalleryOptions />
+      </div>
+    </ProfileFavoriteHeaderContainer>
+  );
+}
