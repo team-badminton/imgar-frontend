9fa1e4a931465f17b57e48a1e138d00ca0e287d6
yeongjongkim <tohero21@naver.com>
2021-10-11 16:49:06 +0900

    feat: grid 레이아웃 적용 및 leftSide 구현 완료
    사유
    1. grid 적용: 접근성 준수를 위해 PostSideVoteBar가 PostContents컴포넌트 다음에 작성했으며
    이를 유연하게 배치하기 위함

--- src/pages/Gallery/Gallery.styled.tsx
+++ src/pages/Gallery/Gallery.styled.tsx
@@ -1,12 +1,63 @@
-import { pxToRem } from '@/util/styleUtils';
 import styled from 'styled-components';
+import { HashLink } from 'react-router-hash-link';
+
+// components
+import { Button, Vote } from '@/components';
+
+// utils
+import { pxToRem } from '@/util/styleUtils';
 
 export const GalleryContainer = styled.div`
+  position: relative;
   margin: 0 ${({ theme }) => theme.spaceSize.l};
-  padding: 0 ${({ theme }) => theme.spaceSize.l};
   ${({ theme }) => theme.spaceSize.l};
   max-width: ${pxToRem(1250)};
   margin-top: ${pxToRem(30)};
-  padding: ${pxToRem(30)};
-  margin: 0 auto ${pxToRem(40)};
+`;
+
+export const PostSideVoteBar = styled.div`
+  position: sticky;
+  top: ${pxToRem(140)};
+  display: flex;
+  width: ${pxToRem(58)};
+  flex-direction: column;
+`;
+
+export const StyledVote = styled(Vote)`
+  border: ${({ theme }) => `${pxToRem(1)} solid ${theme.color.darkGray}`};
+  border-radius: ${({ theme }) => theme.borderRadius.s};
+`;
+
+export const FavoriteButton = styled(Button)`
+  padding: ${({ theme }) => `${theme.spaceSize.r} ${theme.spaceSize.s} ${theme.spaceSize.s}`};
+  .img {
+    transition: transform 0.4s;
+  }
+  &:hover {
+    .img {
+      transform: scale(1.1);
+    }
+    path {
+      stroke: #43d0bd;
+    }
+  }
+`;
+
+export const LinkToComment = styled(HashLink)`
+  display: flex;
+  align-items: center;
+  padding: ${({ theme }) => theme.spaceSize.r};
+  border: ${({ theme }) => `${pxToRem(1)} solid ${theme.color.darkGray}`};
+  border-radius: ${({ theme }) => theme.borderRadius.s};
+  margin-top: ${({ theme }) => theme.spaceSize.xl};
+  flex-direction: column;
+  gap: ${pxToRem(6)};
+  color: ${({ theme }) => theme.color.white};
+  font-size: ${({ theme }) => theme.fontSize.xs};
+
+  svg {
+    width: ${pxToRem(29)};
+    height: ${pxToRem(29)};
+    stroke: ${({ theme }) => theme.color.white};
+  }
 `;
