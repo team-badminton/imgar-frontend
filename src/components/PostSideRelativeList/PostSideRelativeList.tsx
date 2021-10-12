9377848e23a9fd60e6624e99a1bf8a860906691d
yeongjongkim <tohero21@naver.com>
2021-10-12 20:13:43 +0900

    feat: Link 되도록 수정

--- src/components/PostSideRelativeList/PostSideRelativeList.tsx
+++ src/components/PostSideRelativeList/PostSideRelativeList.tsx
@@ -1,11 +1,16 @@
 import React, { ReactElement, useEffect, useRef } from 'react';
-import { Picture } from '..';
 
-// type
+// components
+import { Picture } from '@/components';
+
+// styles
 import { Container, RelativeItem, ImageContainer, Title } from './PostSideRelativeList.styled';
 
 // types
 import { PostSideRelativeListProps } from './PostSideRelativeList.type';
+
+// etc
+import { Link } from 'react-router-dom';
 
 export default function PostSideRelativeList({ title, posts, mainPostId }: PostSideRelativeListProps): ReactElement {
   const ContainerRef = useRef<HTMLUListElement>();
@@ -20,13 +25,15 @@ export default function PostSideRelativeList({ title, posts, mainPostId }: PostS
       <Title>{title}</Title>
       <Container ref={ContainerRef}>
         {posts.map(({ id, title, thumbnailImageId, imageCount }) => (
-          <RelativeItem key={id}>
-            <span className="item-title">{title}</span>
-            <ImageContainer>
-              <Picture imageId={thumbnailImageId} imageHeight={64} imageWidth={64} />
-              <span className="image-count">{imageCount}</span>
-            </ImageContainer>
-          </RelativeItem>
+          <Link key={id} to={`/gallery/${id}`}>
+            <RelativeItem>
+              <span className="item-title">{title}</span>
+              <ImageContainer>
+                <Picture imageId={thumbnailImageId} imageHeight={64} imageWidth={64} />
+                <span className="image-count">{imageCount}</span>
+              </ImageContainer>
+            </RelativeItem>
+          </Link>
         ))}
       </Container>
     </div>
