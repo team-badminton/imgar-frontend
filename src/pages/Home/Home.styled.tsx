206af22d6a8b7159780bb2802a14f5217cd646fd
HyunJung <HyunJungC.Dev@gmail.com>
2021-10-12 20:23:56 +0900

    feat: welcomeMessage와 TagCard 리스트 렌더링
    - welcomeMessage는 API 요청으로 받아오며 그때 그때 다른 message가 출력된다.
    - TagCard 리스트는 MasonryGallery 보다 좀 더 넓은 영역을 차지한다.

--- src/pages/Home/Home.styled.tsx
+++ src/pages/Home/Home.styled.tsx
@@ -1,1 +1,29 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
+import { StyledArticleProps } from '@/pages/Home/Home.type';
+
+export const StyledSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const StyledWelcomeMessage = styled.h2`
+  font-size: ${({ theme }) => theme.fontSize.xxl};
+  color: ${({ theme }) => theme.color.aquaBlue};
+  letter-spacing: 0.6px;
+`;
+
+export const StyledTagHeading = styled.h2`
+  font-size: ${({ theme }) => theme.fontSize.l};
+  color: ${({ theme }) => theme.color.white};
+  width: 100%;
+  margin-left: 4px;
+`;
+
+export const StyledArticle = styled.article<StyledArticleProps>`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: ${({ articleWidth }) => articleWidth + 'px'};
+  padding-bottom: 100px;
+`;
