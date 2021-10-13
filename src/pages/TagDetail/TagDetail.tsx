e1c94a2228b525c14edb3030affd9b466ddaaddb
HyunJung <HyunJungC.Dev@gmail.com>
2021-10-14 01:24:06 +0900

    feat: TagDetail 페이지 카테고리 및 API 요청 수정
    - TagDetail 페이지는 하나의 카테고리만 존재하여 수정
    - 하나의 카테고리에 따라 API 요청하도록 수정

--- src/pages/TagDetail/TagDetail.tsx
+++ src/pages/TagDetail/TagDetail.tsx
@@ -1,39 +1,80 @@
-import { Loading, MainContainer, MasonryGalleryContainer, MasonryGalleryHeader } from '@/components';
-import { useTypedSelector } from '@/redux';
+import { Loading, MainContainer, MasonryGallery } from '@/components';
+import { useTypedDispatch, useTypedSelector } from '@/redux';
 import { useTagPostsQuery } from '@/redux/api';
-import React, { ReactElement } from 'react';
-import { useHistory, useLocation, useParams } from 'react-router-dom';
+import React, { ReactElement, useEffect, useMemo, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { masonryGalleryWidthSelector } from '@/redux/slices/displayReducer';
 import { StyledDiv } from './TagDetail.styled';
+import { setQueryPage } from '@/redux/slices/listInfoReducer';
+import { TagDetailSortOption } from './TagDetail.type';
+import { TagPostsQuery } from '@/redux/api/types/queries';
+import { PostV1Info } from '@/redux/storeTypes';
+import TagDetailHeader from '@/components/TagDetailHeader/TagDetailHeader';
+
+const sortQuery = {
+  popular: 'viral',
+  newest: 'time',
+  best: 'top',
+};
 
 export default function TagDetail(): ReactElement {
-  const { tagname } = useParams<{ tagname: string }>();
-  const { data, isLoading } = useTagPostsQuery({ tagName: tagname });
-
   // 리덕스 상태
-  const galleryWidth = useTypedSelector(masonryGalleryWidthSelector);
+  const masonryGalleryWidth = useTypedSelector(masonryGalleryWidthSelector);
 
-  return isLoading ? (
-    <Loading />
-  ) : (
+  const { tagname } = useParams<{ tagname: string }>();
+  const queryPage = useTypedSelector(state => state.listInfo.queryPage);
+
+  const [posts, setPosts] = React.useState<PostV1Info[]>([]);
+
+  const [sortOption, setSortOption] = useState<TagDetailSortOption>('newest');
+
+  const { data } = useTagPostsQuery({
+    tagName: tagname,
+    page: queryPage,
+    sortOption: sortQuery[sortOption] as TagPostsQuery['sortOption'],
+  });
+  const dispatch = useTypedDispatch();
+  const sortOptionList = useMemo(() => [sortOption], [sortOption]);
+  const setSortOptionList = useMemo(
+    () => [
+      (sortOption: TagDetailSortOption) => {
+        setSortOption(sortOption), dispatch(setQueryPage(1));
+      },
+    ],
+    [setSortOption],
+  );
+
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+    if (queryPage === 1) {
+      setPosts(data.posts);
+    }
+    if (queryPage > 1) {
+      setPosts(currPosts => [...currPosts, ...data.posts]);
+    }
+  }, [data]);
+
+  return data ? (
     <MainContainer
       sticky
       darkenBackground
-      headerBackground={`https://i.imgur.com/${data.backgroundId}.jpg`}
+      headerBackground={data && `https://i.imgur.com/${data.backgroundId}.jpg`}
       headerCover={
         <StyledDiv>
-          <h2>{data.displayName}</h2>
-          <p className="description">{data.description}</p>
-          <p className="postCount">{data.postCount} POSTS</p>
+          <h2>{data?.displayName}</h2>
+          <p className="description">{data?.description}</p>
+          <p className="postCount">{data?.postCount} POSTS</p>
         </StyledDiv>
       }
       customHeader={null}
-      containerWidth={galleryWidth}
+      containerWidth={masonryGalleryWidth}
     >
-      <>
-        <MasonryGalleryHeader />
-        <MasonryGalleryContainer />
-      </>
+      <TagDetailHeader sorted={sortOptionList} setSort={setSortOptionList} />
+      <MasonryGallery posts={posts} />
     </MainContainer>
+  ) : (
+    <Loading />
   );
 }
