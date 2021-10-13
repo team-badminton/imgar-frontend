8969545b729f0048e246921775ab86c91446de43
kimjeongwon <kimjeongwonn@gmail.com>
2021-10-13 17:16:05 +0900

    feat: text내에 이미지나 비디오가 있을경우 변환해 주는 Util함수 제작
    - convertLink의 두번째 인자를 true로 줘서 사용

--- src/util/styleUtils.tsx
+++ src/util/styleUtils.tsx
@@ -1,3 +1,4 @@
+import { Picture, Video } from '@/components';
 import React from 'react';
 import { css } from 'styled-components';
 export function pxToRem(px: number | string): string {
@@ -33,17 +34,62 @@ export function hexToRgb(hex: string, alpha?: number): string {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-export function convertLink(text: string) {
-  return text
+export function convertLink(text: string, withImage?: boolean) {
+  let result: (string | Element | JSX.Element)[] = text
     ?.split(/(https?:\/\/[\w/.\-_=?&#$]+)|(\n)|(#.+)/)
-    .filter(str => str)
-    .map((str, index) =>
-      str.includes('http') ? (
-        <a key={str + index} href={str} target="_blank" rel="noopener noreferrer">
-          {str}
-        </a>
-      ) : (
-        str
-      ),
-    );
+    .filter(str => str);
+
+  if (withImage) {
+    result = result
+      .map((str: string) => {
+        if (str.includes('http')) {
+          if (withImage && str.match(/i\.imgur.*\/.*\.(jpeg|jpg|png)$/)) {
+            const splicedStr = str.split('/');
+            return (
+              <div>
+                <Picture
+                  css={`
+                    margin-top: ${pxToRem(4)};
+                  `}
+                  imageHeight="100"
+                  imageId={splicedStr[splicedStr.length - 1]}
+                />
+              </div>
+            );
+          }
+          if (withImage && str.match(/i\.imgur.*\/.*\.(gif|mp4)$/)) {
+            const splicedStr = str.split('/');
+            return (
+              <div>
+                <Video
+                  css={`
+                    height: ${pxToRem(100)};
+                    margin-top: ${pxToRem(4)};
+                    width: auto;
+                  `}
+                  imageId={splicedStr[splicedStr.length - 1].slice(0, -4)}
+                />
+              </div>
+            );
+          }
+        }
+        return str;
+      })
+      .sort((a, b) => {
+        if (React.isValidElement(a)) return 1;
+        if (React.isValidElement(b)) return -1;
+        return 0;
+      });
+
+    return result.map((str, index) => {
+      if (typeof str === 'string' && str.includes('http')) {
+        return (
+          <a key={str + index} href={str} target="_blank" rel="noopener noreferrer">
+            {str}
+          </a>
+        );
+      }
+      return str;
+    });
+  }
 }
